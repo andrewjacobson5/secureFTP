78c335dc30e5cd0fc859f6145a8cc8131730dc4f
andrew <andrewjacobson12@outlook.com>
2024-10-18 07:24:45 -0400

    Updated main

--- main.py
+++ main.py
@@ -1,38 +1,5 @@
-import json
-import bcrypt
+from user import register_user, user_login
 users = {}
-
-def register_user():
-    username = input("Enter username: ")
-    password = input("Enter password: ")
-    password.strip()
-    username.strip()
-    try:
-        with open('users.json', 'r') as file:
-            existing_users = json.load(file)
-    except FileNotFoundError:
-        existing_users = {}
-
-    existing_users[username] = password
-
-    with open('users.json', 'w') as file:
-        json.dump(existing_users, file, indent=4)
-    
-    print(f"User {username} registered successfully!")
-
-def user_login():
-    username = input("Enter username: ")
-    password = input("Enter password: ")
-    try:
-        with open('users.json', 'r') as file:
-            existing_users = json.load(file)
-    except FileNotFoundError:
-        existing_users = {}
-
-    if username in existing_users and existing_users[username] == password:
-        print(f"User {username} logged in successfully!")
-    else:
-        print("Invalid username or password")
 
 if __name__ == "__main__":
     while True:
